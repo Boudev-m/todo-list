59925483843c9e5eb1261d625462879b0cc58693
bouimust <moussovic@gmail.com>
2022-09-14 11:46:46 +0200

    script de l'application web

--- home.js
+++ home.js
@@ -3,8 +3,9 @@ const tasksList = document.querySelector('.tasks');
 const noTaskMessage = '<p>Aucune tâche à faire pour le moment ...</p>';
 const inputTask = document.getElementsByClassName('input-task')[0];
 const buttonTask = document.getElementsByClassName('button-task')[0];
+document.getElementsByTagName('h1')[0].onclick = function (e) { console.log(e) }
 
-// Vérifie au lancement de l'app si liste des tâches vide
+// Vérifie au lancement de l'app si la liste des tâches est vide
 if (!localStorage.task) {
     tasksList.innerHTML = noTaskMessage;
 } else {
@@ -12,22 +13,38 @@ if (!localStorage.task) {
     showAllTasks(array);
 }
 
+
 // Affiche la liste des tâches
-// (mettre cette fonction à chaque fin de modif de la liste ? pour remettre les number task dans l'ordre)
 function showAllTasks(tasks) {
     for (let i = 0; i < tasks.length; i++) {
-        console.log(tasks[i].name);
         const task = document.createElement('p');    // créer <p>
-        task.innerHTML = `<input type="checkbox" class="task-number-${i}"/>
-        <label>${tasks[i].name}</label>`;            // met le task name dedans
+        task.innerHTML = `<div class="delete-button"></div>
+        <input id=${tasks[i].id} class="checkbox" type="checkbox" ${tasks[i].isDone ? "checked" : ""} />
+        <label for=${tasks[i].id}>${tasks[i].name}</label>`;            // met le task name dedans
         tasksList.appendChild(task);                 // ajoute <p> dans la liste
     }
+    document.querySelector('.button-delete-all').removeAttribute('hidden');
 }
 
+
+// Affiche un exemple de tâche aléatoire dans input
+const exampleTask = [
+    'laver la voiture', 'faire les courses', 'faire du jogging', 'trier le courrier', 'appeler maman',
+    'faire le ménage', 'lire le journal', 'aller au parc', 'préparer le repas', 'réviser le cours'
+]
+function showRandomTask() {
+    const randomTask = "ex : " + exampleTask[Math.floor(Math.random() * exampleTask.length)];
+    inputTask.setAttribute("placeholder", randomTask);
+}
+showRandomTask();
+
+
 // Event pour ajouter une nouvelle tâche
-buttonTask.addEventListener('click', function () {
+document.getElementById('taskForm').addEventListener('submit', function (e) {
+    e.preventDefault();
     !inputTask.value ? alert('Saisissez une tâche') : addNewTask(inputTask.value);
 });
+
 
 // Ajoute une nouvelle tâche
 function addNewTask(task) {
@@ -35,61 +52,103 @@ function addNewTask(task) {
     let array = [];                                 // créer array
     if (localStorage.task) { array = JSON.parse(localStorage.task) }; // local to array
     const taskObject = {                            // créer objet avec new task
+        id: Date.now(),
         name: task,
         isDone: false
     }
     array.push(taskObject);                         // met objet dans array
     const newTask = document.createElement('p');    // créer <p>
-    newTask.innerHTML =                             // met le checkbox et task name dedans
-        `<input type="checkbox" class="task-number-${array.length - 1}"/>
-    <label>${task}</label>`;
+    newTask.innerHTML = `<div class="delete-button"></div>
+        <input id=${Date.now()} class="checkbox" type="checkbox" />
+        <label for=${Date.now()}></label>`;       // met le checkbox et task name dedans
+    newTask.lastChild.textContent = task;           // insère le texte brut dans le label
+    newTask.firstChild.addEventListener('click', e => { deleteTask(e.target) });
+    newTask.children[1].addEventListener('change', e => { changeStateTask(e.target) })
+    // j'ai ajouté les events ici pour qu'ils soient actif immediatement après injection, sans rechargement la page
     tasksList.appendChild(newTask);                 // ajoute <p> dans la liste
     localStorage.task = JSON.stringify(array);      // met array dans local
-    inputTask.value = '';
+    inputTask.value = '';                           // vide le input
+    inputTask.removeAttribute("placeholder");       // supprime le placeholde
+    document.querySelector('.button-delete-all').removeAttribute('hidden');
 }
 
+
+/********************************* */
+
 // Event pour changer l'état d'une tâche
+tasksList.querySelectorAll('.checkbox').forEach(item => {
+    item.addEventListener('change', e => {
+        changeStateTask(item);
+    })
+})
 
 // Change l'état d'une tâche
+function changeStateTask(item) {
+    const array = JSON.parse(localStorage.task);
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].id === parseInt(item.id)) {
+            array[i].isDone = item.checked ? true : false;
+            break;
+        }
+    }
+    localStorage.task = JSON.stringify(array);
+}
 
 // Event pour supprimer une tâche
+document.querySelectorAll('.delete-button').forEach(item => {
+    item.addEventListener('click', e => {
+        deleteTask(item);
+    })
+})
 
 // Supprime une tâche
+function deleteTask(item) {
+    const array = JSON.parse(localStorage.task);
+    console.log(parseInt(item.nextElementSibling.id));
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].id === parseInt(item.nextElementSibling.id)) {
+            console.log('ok ' + [i]);
+            array.splice(i, 1);
+            item.parentElement.remove()
+            break;
+        }
+        console.log(i);
+    }
+    if (!tasksList.firstElementChild) {
+        localStorage.removeItem('task');
+        tasksList.innerHTML = noTaskMessage;
+        document.querySelector('.button-delete-all').setAttribute('hidden', true)
+    }
+    localStorage.task = JSON.stringify(array);
+}
 
 // Event pour supprimer toutes les tâches
+document.querySelector('.button-delete-all').addEventListener('click', function () {
+
+    if (window.confirm('Voulez-vous supprimer toutes des tâches ?')) {
+        localStorage.removeItem("task");
+        tasksList.innerHTML = noTaskMessage;
+        document.querySelector('.button-delete-all').setAttribute('hidden', true)
+    }
+
+});
 
 // Supprime toutes les tâches
 
 
-/*
-array + random :
-ranger la chambre
-faire la vaisselle
-laver la voiture
-réparer le téléphone
-faire les courses
-faire du jogging
-trier le courrier
-appeler maman
-sortir les poubelles
-faire le ménage
-lire le journal
-aller au parc
-aller au restaurant
-réviser le cours
-*/
+
 /********************************************************/
 
 /*
 DOM :
 
-Compter nbre de child d'un elt :
-element.children.length
-element.childElementCount
+Problème : les events ne se declenchent pas après injection HTML, je dois rafraichir la page.
+Objectif : je veux pouvoir changer l'état ou supprimer une tâche toute de suite après l'avoir créer, sans recharger la page
+Solution : il faut intégrer les events dans le scope d'innerHTML
 
-innerHTML : pour injecter/ajouter/modifier une balise avec du contenu
-textContent : pour injecter/ajouter/modifier du contenu
-innerText : ?
+Ne pas utiliser innerHTML pour inserer du contenu dans la page.
+risque d'attaque en insérant des balises html
+il faut utiliser textContent.
 
 JS :
 
@@ -108,6 +167,14 @@ Extrapolation :
 
 ! operator = empty or false
 
-Suppression d'un elt du DOM :
-elt.remove()
+Date.now() = type number, nbre de ms écoulées depuis le 01/01/1970 00:00:00 UTC
+
+
+// RANDOM VALUE FROM ARRAY
+const months = ["January", "February", "March", "April", "May", "June", "July"];
+const random = Math.floor(Math.random() * months.length);
+console.log(random);  // random from 0 to 6
+console.log(months[random]); // random from january to july
+raccourci : months[Math.floor(Math.random() * months.length)];
+
 */
